cdadedc9d2fbec91ff2b7e90c15471f7c51cd329
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-02-12 10:00:50 +0530

    created and initilized google firebase

---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7ylMU4vgUn1d3OMlvIOvA5Y6NE3JAvwU",
+  authDomain: "jigyasajunction-4212e.firebaseapp.com",
+  projectId: "jigyasajunction-4212e",
+  storageBucket: "jigyasajunction-4212e.appspot.com",
+  messagingSenderId: "585842859248",
+  appId: "1:585842859248:web:6fbec53da6045a14907635"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
